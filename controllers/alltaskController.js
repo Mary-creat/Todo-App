a8b8df8530c7421afc303824c6b760a82497a368
Mary <adesholaakintunde@gmail.com>
2025-07-26 00:54:03 -0400

    God is good always

--- controllers/alltaskController.js
+++ controllers/alltaskController.js
@@ -1,22 +1,7 @@
-const db = require('../config/mongoose');
-const Dashboard = require('../models/dashboard');
-const User = require('../models/register');
+const userId = req.session.userId; // this comes from login
 
-module.exports.alltask = function(req, res){
-    const data = Dashboard.find({})
-    .then(function(data){
-        User.findOne({email : "ankitvis609@gmail.com"})
-        .then(function(user){
-            console.log(`**********user`, user.name);
-        return res.render('alltask', {
-            title: "Dashboard",
-            name: user.name,
-            dashboard: data
-        });
-    })
-    })
-    .catch(function(err){
-        console.log('Error', err);
-        return;
-    });
+if (!userId) {
+  return res.redirect('/login');
 }
+
+const user = await User.findById(userId);
